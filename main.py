4c342432de01fede59d069fb604a9ed74471c6da
Patrick Kraus-Füreder <krausp86@gmail.com>
2018-08-03 13:10:33 +0200

    In diesem Update wurde eine einfache Variante des genetischen Algo implementiert. Das Skript findet in den meisten Fällen das optimum.

--- main.py
+++ main.py
@@ -3,18 +3,29 @@
 # Hauptskript, in welchem der genetische Algorithmus realisiert wird.
 #
 # Patrick Kraus-Fuereder
-# 02.08.2018
+# E 02.08.2018
+#
+# M 03.08.2018 - Erweitert auf 8 Bit
+# M 03.08.2018 - Elementare Genetik
 
 from Regressor import *
-from numpy import NaN
+import matplotlib.pyplot as plt
+
+ZSeed = int(floor(rand()*255))
+ZSeed = fromstring(binary_repr(ZSeed), dtype='S1').astype(int)[::-1]
+Ziel = array([0,0,0,0,0,0,0,0])
+Ziel[0:ZSeed.size] = ZSeed
+Para = 20*(rand(1,sum(Ziel))-0.5)
+print(Ziel)
+print(Para)
 
 def linfun(XX):
     # Polynomfunktion, welche durch den Regressor moeglichst nahe angenaehert werden sollte.
     # Werte sollten zwischen -10 und 10 liegen.
     # P.K.-F. (02.08.2018)
     
-    AIM = array([0,1,1,0,0,1])
-    Par = array([1.2,-0.3,0.004])
+    AIM = Ziel#array([0,1,1,0,0,1,0,0])
+    Par = Para#array([1.2,-0.3,0.004])
 
     whe = argwhere(AIM).T
     X = zeros([XX.size,whe.size])
@@ -23,31 +34,79 @@ def linfun(XX):
     
     return sum(X,1)
 
+def recombine(Genpol):
+    # Diese Funktion rekombiniert die besten 10 individuen um wieder einen 
+    # Genpool von 100 individuen zu schaffen.
+    GENEPOOL = zeros([100,8])
+    for parent_1 in range(Genpol.shape[0]):
+	for parent_2 in range(Genpol.shape[0]):
+	    pos = parent_1*10 + parent_2
+	    for bitpos in range(Genpol.shape[1]):
+		B = 0
+                if Genpol[parent_1,bitpos]==Genpol[parent_2,bitpos]:
+		    B = Genpol[parent_1,bitpos]
+                else:
+                    r = rand()
+                    B = 0 if r<0.5 else 1
+                # mutationsschritt
+                r = rand()
+                if r<0.005:
+		    #print("Mutation!")
+                    if B==1:
+                        B = 0
+                    else:
+                        B = 1
+		GENEPOOL[pos,bitpos] = B
+    return GENEPOOL
 
+# Initiiere ueberblicksarray ueber die Generationen
+GenerationP = []
 
-GENEPOOL = rand(300,6)
+GENEPOOL = rand(100,8)
 GENEPOOL[GENEPOOL<0.5] = 0
 GENEPOOL[GENEPOOL>0] = 1
 
-G_List = zeros([GENEPOOL.shape[0],1])
+z = Ziel[::-1]
+print("Zielgen: ",z.dot(2**arange(z.size)[::-1]))
 
-N = 1000
-X_Train = rand(N)*20-10
-Y_Train = linfun(X_Train) + rand(X_Train.size)
+for gen in range(10):
+    if gen%1==0:
+        print("Generation: ",gen)
+    G_List = zeros([GENEPOOL.shape[0],1])
+    
+    N = 50
+    X_Train = rand(N)*20-10
+    Y_Train = linfun(X_Train) + rand(X_Train.size)
+    
+    X_Test = rand(100)*20-10
+    Y_Test = linfun(X_Test)
+    
+    GenNow = []
+    for GEN_I in range(GENEPOOL.shape[0]):
+        G = GENEPOOL[GEN_I,:]
+        if not sum(G)==0:
+            Reg = Regressor(GENEPOOL[GEN_I,:])
+            Reg.learn(X_Train,Y_Train)
+            R = sum((Reg.eval(X_Test)-Y_Test)**2)
+        else:
+            R = 1E16
+        G = G[::-1]
+        GenNow.append(G.dot(2**arange(G.size)[::-1]))
+        G_List[GEN_I] = R
 
-X_Test = rand(100)*20-10
-Y_Test = linfun(X_Test)
+    GenerationP.append(GenNow)
 
-for GEN_I in range(GENEPOOL.shape[0]):
-    print(GENEPOOL[GEN_I,:])
-    if not sum(GENEPOOL[GEN_I,:])==0:
-        Reg = Regressor(GENEPOOL[GEN_I,:])
-        Reg.learn(X_Train,Y_Train)
-        R = sum((Reg.eval(X_Test)-Y_Test)**2)
-    else:
-        R = 1E16
-    G_List[GEN_I] = R
+    minind = argsort(G_List.T).T
+    GENEOPT = GENEPOOL[minind[0:10],:].squeeze()
+    GENEPOOL = recombine(GENEOPT)
 
-minind = argmin(G_List)
-print(G_List[minind])
-print(GENEPOOL[minind,:])
+    plt.figure()
+    plt.hist(GenNow)
+    plt.xlim([0,256])
+
+GenerationP = array(GenerationP)
+#plt.figure("Vorher")
+#plt.hist(GenerationP[0,:])
+#plt.figure("Nachher")
+#plt.hist(GenerationP[-1,:])
+plt.show()
